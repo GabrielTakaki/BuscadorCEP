1473b447748d5691ae50a47cdd7c01bb51761de1
Gabriel Takaki <gabitj@icloud.com>
2021-12-07 17:25:54 -0300

    footer test

--- cypress/integration/footer_spec.js
+++ cypress/integration/footer_spec.js
@@ -7,29 +7,12 @@ describe('Tem os elementos corretos no footer', () => {
   it('Apresenta logo do github e linkedin', () => {
     cy.visit('http://localhost:3000/');
     
-    cy.get('.img-logo').should('be.visible');
+    cy.get('#github-link').should('be.visible');
+    cy.get('#linkedin-link').should('be.visible');
   });
 
-  it('Apresenta os botões de redirecionamento', () => {
+  it('Apresenta copyright', () => {
     cy.visit('http://localhost:3000/');
-    cy.get('#home-link').should('be.visible');
-    cy.get('#zip-link').should('be.visible');
-    cy.get('#address-link').should('be.visible');
-  });
-});
-
-describe('Os botões tem o comportamento esperado', () => {
-  it('Se redireciona para as páginas esperadas', () => {
-    cy.visit('http://localhost:3000/');
-    cy.get('#home-link').click();
-    cy.url().should('eq', 'http://localhost:3000/');
-
-    cy.visit('http://localhost:3000/');
-    cy.get('#zip-link').click();
-    cy.url().should('eq', 'http://localhost:3000/findByZip');
-
-    cy.visit('http://localhost:3000/');
-    cy.get('#address-link').click();
-    cy.url().should('eq', 'http://localhost:3000/findByAddress');
+    cy.get('.copyright').should('be.visible');
   });
 });
