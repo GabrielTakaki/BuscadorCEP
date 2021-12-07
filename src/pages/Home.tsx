6d4e9a8e62c82f31160eed6b48d6e124a2bd5902
Gabriel Takaki <gabitj@icloud.com>
2021-12-07 11:41:39 -0300

    add content to the main tag at home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,10 @@ const Home: React.FC = () => {
   return (
     <>
       <Header />
-      <main></main>
+      <main className="main">
+        <h3 className="main__title">Seja bem-vindo ao BuscadorCEP!</h3>
+        <p className="main__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic qui ipsam, tempora animi voluptatum molestiae eum et nemo ad voluptas officiis. Sequi, perspiciatis? Ratione quibusdam similique nemo id voluptas incidunt labore. Aut eveniet veniam dicta natus obcaecati iusto excepturi quam est nihil, nesciunt officiis repellat beatae dolores facere doloremque animi!</p>
+      </main>
       <Footer />
     </>
   );
